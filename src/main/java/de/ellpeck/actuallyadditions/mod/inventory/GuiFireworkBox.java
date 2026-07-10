6119de2959cdefc9450a01941b8594fe9dc75db9
MisterIceCat <mistericecat@gmail.com>
2026-07-10 15:55:49 +0200

    Fix GUI Background of the FireworkBox

--- src/main/java/de/ellpeck/actuallyadditions/mod/inventory/GuiFireworkBox.java
+++ src/main/java/de/ellpeck/actuallyadditions/mod/inventory/GuiFireworkBox.java
@@ -80,6 +80,13 @@ public class GuiFireworkBox extends GuiContainer implements GuiResponder {
     }
 
     @Override
+    public void drawScreen(int mouseX, int mouseY, float partialTicks) {
+        this.drawDefaultBackground();
+        super.drawScreen(mouseX, mouseY, partialTicks);
+        this.renderHoveredToolTip(mouseX, mouseY);
+    }
+
+    @Override
     public void setEntryValue(int id, String value) {
 
     }
